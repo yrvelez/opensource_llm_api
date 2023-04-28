48b62ad16c4c547b677ff47176a06148e6f3d4b7
yrvelez <52504034+yrvelez@users.noreply.github.com>
2023-04-28 12:45:29 +0000

    Save full output

--- main.py
+++ main.py
@@ -3,39 +3,38 @@ import replicate
 import re
 import os
 
-rep_client = replicate.Client(api_token= os.environ['replicate_api'])
+rep_client = replicate.Client(api_token=os.environ['replicate_api'])
 app = Flask(__name__)
+
 
 @app.route('/predict')
 def predict():
-  
-    user_text = request.args.get('input', '')
-    instruction = request.args.get('instruction', '')
-    model = request.args.get('model', '')
-    
-    prompt = 'instruction: ' + instruction + '\ninput: ' + user_text + '\noutput:'
 
-    response = rep_client.run(model, input={"prompt": prompt, 
-                                            "max_length": 100})
+  user_text = request.args.get('input', '')
+  instruction = request.args.get('instruction', '')
+  model = request.args.get('model', '')
 
-    # Output is returned as a stream
-    output = []
-  
-    for text in response:
-        output.append(text)
-  
-    # split the response into sentences
-    sentences = re.split("(?<=[.!?]) +", ''.join(output))
+  prompt = 'instruction: ' + instruction + '\ninput: ' + user_text + '\noutput:'
 
-    # remove any partial sentences that do not end with a punctuation mark
-    sentences = [s for s in sentences if re.search("[.!?]$", s)]
+  # Output is returned as a stream
+  output = []
 
-    # join the remaining sentences back into a string
-    final_response = ' '.join(sentences)
+  response = rep_client.run(model, input={"prompt": prompt, "max_length": 100})
+
+  output = list(response)
   
-    return jsonify({'response': final_response})
+  # split the response into sentences
+  sentences = re.split(r'(?<=[\n.!?])\s+', ''.join(output))
+
+  # remove any partial sentences that do not end with a punctuation mark
+  sentences = [s for s in sentences if re.search("[.!?]$", s)]
+
+  # join the remaining sentences back into a string
+  final_response = ' '.join(sentences)
+
+  return jsonify({'response': final_response})
+
 
 # Run the app
 if __name__ == '__main__':
-    app.run(debug=True,
-           host = '0.0.0.0')
+  app.run(debug=True, host='0.0.0.0')
